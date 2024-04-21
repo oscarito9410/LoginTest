a91cb3b506e3aa71b043df8a38012552516f3d81
OSCAR EMILIO PÉREZ MARTÍNEZ <13366923+oscarito9410@users.noreply.github.com>
2024-04-21 15:38:02 -0600

    ktlint format

--- androidApp/src/main/java/com/aboolean/logintest/android/MainActivity.kt
+++ androidApp/src/main/java/com/aboolean/logintest/android/MainActivity.kt
@@ -3,9 +3,7 @@ package com.aboolean.logintest.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.*
-import androidx.compose.ui.Modifier
 import com.aboolean.logintest.presentation.screens.login.LoginScreen
 
 class MainActivity : ComponentActivity() {
